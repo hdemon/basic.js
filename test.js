af953b36fd8b5cabfc9d3cf60bc2efbfcc752bc6
Masami Yonehara <zeitdiebe@gmail.com>
2014-12-03 23:05:26 +0900

    remove unneeded file

--- test.js
+++
@@ -1,7 +0,0 @@
-    var MeCab = new require('mecab-async')
-      , mecab = new MeCab()
-    ;
-    mecab.parse('いつもニコニコあなたの隣に這い寄る混沌ニャルラトホテプです！', function(err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
